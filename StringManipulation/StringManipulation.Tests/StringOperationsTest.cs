4ad6a552a155493a902fbcc1d091012845deeee0
Raymundo Solis <pepperray01@gmail.com>
2025-02-03 16:25:45 -0600

    Course xUnit | Types of asserts

--- StringManipulation/StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation/StringManipulation.Tests/StringOperationsTest.cs
@@ -5,8 +5,45 @@ public class StringOperationsTest
     [Fact]
     public void ConcatenateStringsTest()
     {
+        // Arrange
         var strOperations = new StringOperations();
+        
+        // Act
         var result = strOperations.ConcatenateStrings("Hello", "World");
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
         Assert.Equal("Hello World", result);
     }
+
+    [Fact]
+    public void IsPalindromeTestIsTrue()
+    {
+        // Arrange
+        var strOperations = new StringOperations();
+        
+        // Act
+        var result = strOperations.IsPalindrome("ama");
+        
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsPalindromeTestIsFalse()
+    {
+        var strOperations = new StringOperations();
+        var result = strOperations.IsPalindrome("hello");
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void RemoveWhiteSpaceTest()
+    {
+        var strOperations = new StringOperations();
+        var result = strOperations.RemoveWhitespace(" Hola, esto es una prueba ");
+        Assert.DoesNotContain(" ", result);
+    }
+    
 }
